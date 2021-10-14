a76a4d4e7826fb27802b038eb959002522a16c6a
lalo <lalopx555@gmail.com>
2021-10-14 14:20:38 -0500

    Updates

---
+++ app.js
@@ -0,0 +1,38 @@
+//@ts-check
+
+import {questions} from './data/questions.js';
+import {Quiz} from './models/quiz.js';
+import {UI} from './models/UI.js'
+
+
+/**
+ * 
+ * @param {Quiz} quiz quiz object
+ * @param {UI} ui ui objetc
+ */
+
+const renderPage = (quiz, ui) => {
+    if (quiz.isEnded()) {
+        console.log(quiz.score);
+        ui.showScores(quiz.score)
+    }else{
+    ui.showQuestion(quiz.getQuestionIndex().text);
+    ui.showChoices(quiz.getQuestionIndex().choices, (currentChoice) => {
+    quiz.guess(currentChoice);
+    renderPage(quiz, ui);
+        });
+
+    ui.showProgress(quiz.questionIndex + 1, quiz.questions.length); 
+    }
+};
+
+function main() {
+    const quiz = new Quiz(questions)
+    const ui = new UI()
+
+    
+
+    renderPage(quiz, ui)
+}
+
+main();
---
+++ data/data.js
@@ -0,0 +1,28 @@
+
+
+export const data= [
+{
+    question: 'Cual de las siguientes opciones no es un pokemon?',
+    choices: ['charizard','pikachu','mip','chicorita'],
+    answer: 'mip'
+},
+
+{
+question: 'Cual de estas opciones no es un pokemon electrico?',
+    choices: ['Zapdos','pikachu','Magnamite','chicorita'],
+    answer: 'chicorita'
+},
+
+{
+    question: 'Cual de estas opciones no es un pokemon de agua?',
+        choices: ['articuno','psyduck','squirtle','magikarp'],
+        answer: 'articuno'
+    },
+
+    {
+        question: 'Cual de estos pokemon fue encontrado por ash ketchup bajo la lluvia? ?',
+            choices: ['moltres','charmander','squirtle','onix'],
+            answer: 'charmander'
+        }
+
+]
