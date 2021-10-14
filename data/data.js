52d92bf932409420f5039806972219a2133ce201
lalo <lalopx555@gmail.com>
2021-10-14 16:27:48 -0500

    10

--- data/data.js
+++ data/data.js
@@ -23,6 +23,44 @@ question: 'Cual de estas opciones no es un pokemon electrico?',
         question: 'Cual de estos pokemon fue encontrado por ash ketchup bajo la lluvia? ?',
             choices: ['moltres','charmander','squirtle','onix'],
             answer: 'charmander'
-        }
+        },
+
+        {
+            question: 'Como evoluciona pikachu ?',
+                choices: ['Con una piedra trueno','Batallas','No puede evolucionar','Con caramelos'],
+                answer: 'Con una piedra trueno'
+            },
+
+            {
+                question: 'Cuantos pokemon pueden hablar?',
+                    choices: ['3','No pueden hablar','Solo miau', 'Graveler'],
+                    answer: 'Solo miau'
+                },
+
+                {
+                    question: 'Que tipo de pokemon es ditto?',
+                        choices: ['Bicho','Normal','Fuego', 'Metamorfo'],
+                        answer: 'Metamorfo'
+                    },
+
+                    {
+                        question: 'Es un pokemon legendario',
+                            choices: ['Lugia','Magikarp','Vulpix', 'Gastly'],
+                            answer: 'Lugia'
+                        },
+
+                        {
+                            question: 'Es un integrante del equipo rocket',
+                                choices: ['Brooke','Profesor Oak','James', 'Misty'],
+                                answer: 'James'
+                            },
+
+                            {
+                                question: 'Como se atrapa a los pokemon?',
+                                    choices: ['Con redes','A palos','Con una pokebola','Con una poketrampa'],
+                                    answer: 'Con una pokebola'
+                                }
+
+
 
 ]
