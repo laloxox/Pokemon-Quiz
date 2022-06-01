8ab014ce3269bbbe3d7562fa19df6090c8c7b6d8
Eduardo <lalopx555@gmail.com>
2022-06-01 15:27:27 -0500

    Mofidicaciones de diseño en la pagina en general

--- data/data.js
+++ data/data.js
@@ -1,90 +1,106 @@
 
 
 export const data= [
+
+    //Question #1
 {
     question: 'Cual de las siguientes opciones no es un pokemon?',
     choices: ['Charizard','Pikachu','Mip','Chicorita'],
     answer: 'Mip'
 },
 
+    //Question #2
 {
 question: 'Cual de estas opciones no es un pokemon electrico?',
     choices: ['Zapdos','Pikachu','Magnamite','Chicorita'],
     answer: 'Chicorita'
 },
 
+    //Question #3
 {
     question: 'Cual de estas opciones no es un pokemon de agua?',
         choices: ['Articuno','Psyduck','Squirtle','Magikarp'],
         answer: 'Articuno'
     },
 
+        //Question #4
     {
         question: 'Cual de estos pokemon fue encontrado por ash ketchup bajo la lluvia? ?',
             choices: ['Moltres','Charmander','Squirtle','Onix'],
             answer: 'Charmander'
         },
 
+            //Question #5
         {
             question: 'Como evoluciona pikachu ?',
                 choices: ['Con una piedra trueno','Batallas','No puede evolucionar','Con caramelos'],
                 answer: 'Con una piedra trueno'
             },
 
+                //Question #6
             {
                 question: 'Cuantos pokemon pueden hablar?',
                     choices: ['3','No pueden hablar','Solo miau', 'Graveler'],
                     answer: 'Solo miau'
                 },
 
+                    //Question #7
                 {
                     question: 'Que tipo de pokemon es ditto?',
                         choices: ['Bicho','Normal','Fuego', 'Metamorfo'],
                         answer: 'Metamorfo'
                     },
 
+                        //Question #8
                     {
                         question: 'Es un pokemon legendario',
                             choices: ['Lugia','Magikarp','Vulpix', 'Gastly'],
                             answer: 'Lugia'
                         },
 
+                            //Question #9
                         {
                             question: 'Es un integrante del equipo rocket',
                                 choices: ['Brooke','Profesor Oak','James', 'Misty'],
                                 answer: 'James'
                             },
 
+                                //Question #10
                             {
                                 question: 'Como se atrapa a los pokemon?',
                                     choices: ['Con redes','A palos','Con una pokebola','Con una poketrampa'],
                                     answer: 'Con una pokebola'
                                 },
 
+                                    //Question #11
                                 {
                                     question: 'Como es que ash consiguio a pikachu?',
                                     choices: ['Porque se lo encontro','lo eligio','Porque llego tarde a elegir un pokemon','se lo regalaron'],
                                     answer: 'Porque llego tarde a elegir un pokemon'
                                 },
 
+                                    //Question #12
                                 {
                                     question: 'Es un pokemon que surgio a traves de exponer el ADN de un virus espacial a un rayo laser? ',
                                     choices: ['Hitmonchan','Graveler','pichu','Deoxys'],
                                     answer: 'Deoxys'
-                                }, ///
+                                }, 
 
+                                    //Question #13
                                 {
                                     question: 'En la serie, como logran mover a snorlaks despues de quedarse dormido?',
                                     choices: ['Con unos troncos','Con una carretilla','Con cuerdas','Juan'],
                                     answer: 'Con unos troncos'
                                 },
 
+                                        //Question #14
                                 {
                                     question: 'Como se llama el pokemon que misty siempre lleva en sus brazos?',
                                     choices: ['Hitmonchan','Abra','Gaslty','Togepi'],
                                     answer: 'Togepi'
                                 },
 
+                                        //Question #15
                                 {
                                     question: 'Cual es el nombre del dispositivo que usan para saber la informacion de un pokemon?',
                                     choices: ['Pokepedia','Pokedex','Base de Datos Pokemon ','El civer'],
